7c6bd85fa219ea262cb2c56909b1967817098669
JaclynFox <54548987+JaclynFox@users.noreply.github.com>
2021-04-21 17:25:13 -0500

    Add project files.

---
+++ LambdaUnitTestExample/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaUnitTestExample
+{
+    public enum Condition : int
+    {
+        LEMON = 0,
+        BAD = 1,
+        FAIR = 2,
+        GOOD = 3,
+        EXCELLENT = 4
+    }
+    public class Car
+    {
+        private Condition _condition;
+        private int _speed;
+        private string _make;
+        public Condition Condition { get => _condition; set => _condition = value; }
+        public int Speed { get => _speed; set => _speed = SetSpeed(value); }
+        public string Make { get => _make; set => _make = value; }
+        public Car(string m, Condition c)
+        {
+            Make = m;
+            Condition = c;
+            Speed = 0;
+        }
+        public int SetSpeed(int s)
+        {
+            int r = s % 5;
+            return s - r;
+        }
+    }
+}
---
+++ LambdaUnitTestExample/Function.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace LambdaUnitTestExample
+{
+    public class Function
+    {
+        public string FunctionHandler(string input, ILambdaContext context)
+        {
+            return input?.ToUpper();
+        }
+    }
+}
